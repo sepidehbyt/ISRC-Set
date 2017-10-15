b3fd3c2b46d7d6ba247f88fe94998269d46472c8
Ramtin <r.shakeri1375@gmail.com>
2017-10-15 16:41:48 +0330

    initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+Global variables
+"""
+
+http_address = '127.0.0.1'
+http_port = 5000
+mongodb_address = '127.0.0.1'
+mongodb_port = 27017
+broker_address = 'iot.ceit.aut.ac.ir'  # '127.0.0.1'
+broker_port = 58904  # 9998
+
+max_id = 10
+agent_connections = [False] * max_id
---
+++ mqtt_handler.py
@@ -0,0 +1,29 @@
+"""
+MQTT Handler
+"""
+
+import paho.mqtt.client as mqtt
+from config import broker_address, broker_port
+
+
+def on_message(client, userdata, message):
+    pass
+
+
+def on_publish(client, userdata, result):
+    pass
+
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe('set')  # we subscribe on 'set'
+
+
+def on_disconnect(client, userdata, rc):
+    pass
+
+
+client = mqtt.Client('set')  # Defining a client named 'set'
+client.on_message = on_message
+client.on_publish = on_publish
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
