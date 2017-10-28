6a73c77602434fa57752d7863d0fa0533d80d5e4
Sepideh <sepideh.byt@gmail.com>
2017-10-28 16:40:45 +0330

    wrong file was uploaded ! :D

--- config.py
+++ config.py
@@ -2,13 +2,10 @@
 Global variables
 """
 
-mongodb_address = '127.0.0.1'
-mongodb_port = 27017
-broker_address = 'iot.eclipse.org'  # '127.0.0.1'
-broker_port = 1883  # 9998
+http_address = '0.0.0.0'
+http_port = 5000
+broker_address = os.environ['MQTT_broker']
+broker_port = 1883
 
-db_name = 'push'
-collection_name = 'log'
-
-client_name = 'push'
-subscribe_name = 'push'
+client_name = 'set'
+publish_name = 'broker'
